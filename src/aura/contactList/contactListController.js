089b23e64b47adee19ae4438f08e12002e87b2ce
rania <rania.zeineldin@gmail.com>
2017-11-10 12:26:20 -0500

    contacts to show up

--- src/aura/contactList/contactListController.js
+++ src/aura/contactList/contactListController.js
@@ -1,5 +1,15 @@
 ({
-	myAction : function(component, event, helper) {
+	dataGetter : function(component, event, helper) {
+		var action = component.get("c.getContacts") // connects to the controller in "contactController.cls"
 		
+		action.setCallback(this, function (response) {  // call back fucntion requests info from back end to set to varibale for manipulation
+			component.set("v.contacts", response.getReturnValue());  // variale "contacts" and what to do when you get it
+			console.log("before action", response.getReturnValue());
+			console.info(response.getReturnValue(), "look here");
+
+		});
+
+		$A.enqueueAction(action);  // this fires the action after callback
 	}
-})
+
+});
